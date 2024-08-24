8684b7d539f25f0b13199b4147116330c7e2c9ff
suk-6 <me@suk.kr>
2024-08-25 07:03:51 +0900

    chore: Add AWS S3 configuration validation to config.validator.ts

--- src/common/validators/config.validator.ts
+++ src/common/validators/config.validator.ts
@@ -4,4 +4,10 @@ export const ConfigValidator = Joi.object({
   NODE_ENV: Joi.string()
     .valid("development", "production", "test")
     .default("production"),
+  // AWS S3 Configuration
+  S3_ENDPOINT: Joi.string().required(),
+  S3_BUCKET_NAME: Joi.string().required(),
+  S3_REGION: Joi.string().required(),
+  S3_ACCESS_KEY: Joi.string().required(),
+  S3_SECRET_KEY: Joi.string().required(),
 });
